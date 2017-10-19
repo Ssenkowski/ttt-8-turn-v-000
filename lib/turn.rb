a6ec625c63846b565f0f941b2b85759f2638cba0
Ssenkowski <scsenkowski@gmail.com>
2017-10-19 13:33:10 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -18,8 +18,8 @@ def input_to_index(input)
 end
 
 def valid_move?(board, index)
-  if index <= 8 && index >= 0 == true 
-  if  position_taken?(board, index) == true 
+  if index <= 8 && index >= 0 == true
+  if  position_taken?(board, index) == true
      false
   else
      true
