8522e02d7e5b50e50ff50f3a36922ddfc49ff194
Ssenkowski <scsenkowski@gmail.com>
2017-10-19 12:51:17 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -32,7 +32,5 @@ def turn(board)
   valid_move?([" "," "," "," "," "," "," "," "," "], 0) == true
   if valid_move?([" "," "," "," "," "," "," "," "," "], 0) == false
     puts "Please enter 1-9:"
-    user_input = gets.strip
-    input_to_index(user_input)
   end
 end
